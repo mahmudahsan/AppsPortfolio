0c018c5ecdc921b9a0242e1589499a046d7945df
Mahmud Ahsan <mahmud@thinkdiff.net>
2022-12-17 13:12:48 +0800

    Update

--- Package.swift
+++ Package.swift
@@ -23,8 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AppsPortfolio",
-            path: "Sources",
-            exclude: ["Demo"]),
+            path: "Sources"),
         .testTarget(
             name: "AppsPortfolioTests",
             dependencies: ["AppsPortfolio"]),
--- Tests/AppsPortfolioTests/AppsPortfolioTests.swift
+++ Tests/AppsPortfolioTests/AppsPortfolioTests.swift
@@ -6,6 +6,6 @@ final class AppsPortfolioTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(AppsPortfolio().text, "Hello, World!")
+        XCTAssertEqual("Hello, World!", "Hello, World!")
     }
 }
